f0dded55567fb5ae7fdc8e71df9432508d171da2
JakubOpyd <jakubopyd@gmail.com>
2025-02-13 23:47:22 +0100

    chore: rename components from .js to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import MainLayout from './layouts/MainLayout'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
 function App() {
 
   return (
-    <MainLayout/>
+    <Router>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
+    </Routes>
+  </Router>
   );
 }
 
