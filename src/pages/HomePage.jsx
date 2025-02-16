fb8133a4c1b6906e3e02f8ec6c0db97998caf0ca
JakubOpyd <jakubopyd@gmail.com>
2025-02-16 17:45:34 +0100

    feat: create homepage layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,21 @@
+import banner_for_home_page from "../assets/banner_for_home_page.png";
 function HomePage() {
-    return ( <h1>Home page</h1> );
+  return (
+    <>
+      <div className="relative w-full p-20 ">
+        <img
+          src={banner_for_home_page}
+          alt="Banner"
+          className="w-full h-full object-cover rounded-lg"
+        />
+        <button className="bg-black absolute bottom-34 left-1/2 transform font-extrabold -translate-x-1/2 scale-200 text-green-400 px-4 py-2 rounded-lg hover: cursor-pointer">
+          Zacznij teraz
+        </button>
+      </div>
+
+       <span>Przekonaj się co możesz osiągnąć z Under the lens</span>
+    </>
+  );
 }
 
 export default HomePage;
